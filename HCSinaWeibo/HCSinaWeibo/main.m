8515044d7b9a4853fa90a51fd7d80d0f6d478141
tunnyios <shc245245@126.com>
2015-06-16 14:28:24 +0800

    初始化项目

---
+++ HCSinaWeibo/HCSinaWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HCSinaWeibo
+//
+//  Created by suhongcheng on 15/6/16.
+//  Copyright (c) 2015年 tunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
